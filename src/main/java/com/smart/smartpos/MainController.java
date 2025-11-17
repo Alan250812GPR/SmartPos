45766e6c54350d87d380469f0c42361d55d9eaa3
alanparra25 <alan.parra@samaya.mx>
2025-11-16 23:25:41 -0600

    AP-16112025-Nueva interfaz

--- src/main/java/com/smart/smartpos/MainController.java
+++ src/main/java/com/smart/smartpos/MainController.java
@@ -1,14 +1,39 @@
 package com.smart.smartpos;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 public class MainController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TextField UserField;
+    @FXML
+    private PasswordField passwordField;
+    @FXML
+    private Button loginButton;
+    @FXML
+    private Hyperlink forgotPasswordLink;
+    @FXML
+    private Hyperlink signUpLink;
+
+    @FXML
+    public void initialize() {
+        if (loginButton != null) {
+            loginButton.setOnAction(event -> handleLoginButtonAction());
+        }
+    }
+
+    private void handleLoginButtonAction() {
+        String email = UserField.getText();
+        String password = passwordField.getText();
+        System.out.println("Email: " + email + ", Password: " + password);
+    }
+
+    @FXML
+    private void onSignUpClick() {
+        System.out.println("Sign Up clicked!");
     }
 }
